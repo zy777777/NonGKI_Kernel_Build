4c75ae148a98abd38f3b23dbda13f7e8fd087cfa
JackA1ltman <cs2dtzq@163.com>
2025-03-03 17:54:00 +0800

    Added a new patch to address issues in lower-version kernels where KernelSU fails to install and uninstall modules, as well as issues with PM (Power Management) execution errors. This patch has been initially applied to the Mix 2s.

---
+++ Patches/extra_patches.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Patches author: weishu <twsxtd@gmail.com>
+#                 F-19-F @ Github
+# Shell authon: JackA1ltman <cs2dtzq@163.com>
+# Tested kernel versions: 4.9
+# 20250303
+
+patch_files=(
+    fs/devpts/inode.c
+    security/selinux/hooks.c
+)
+
+for i in "${patch_files[@]}"; do
+
+    if grep -q "ksu" "$i"; then
+        echo "Warning: $i contains KernelSU"
+        continue
+    fi
+
+    case $i in
+
+    # fs/ changes
+    ## fs/devpts/inode.c
+    s/devpts/inode.c)
+    sed -i -e '/void \*devpts_get_priv(struct dentry \*dentry)/i\extern int ksu_handle_devpts(struct inode*);\n' \
+       -e '/if (dentry->d_sb->s_magic != DEVPTS_SUPER_MAGIC)/i\    ksu_handle_devpts(dentry->d_inode);'
+       fs/devpts/inode.c
+        ;;
+
+    # security/ changes
+    ## security/selinux/hooks.c
+    security/selinux/hooks.c)
+    sed -i -e '/int nnp = (bprm->unsafe & LSM_UNSAFE_NO_NEW_PRIVS);/i\    static u32 ksu_sid;\n    char *secdata;' \
+       -e '/if (!nnp && !nosuid)/i\    int error;\n    u32 seclen;\n' \
+       -e '/return 0; \/\* No change in credentials \*\//a\\n    if (!ksu_sid)\n        security_secctx_to_secid("u:r:su:s0", strlen("u:r:su:s0"), &ksu_sid);\n\n    error = security_secid_to_secctx(old_tsec->sid, &secdata, &seclen);\n    if (!error) {\n        rc = strcmp("u:r:init:s0", secdata);\n        security_release_secctx(secdata, seclen);\n        if (rc == 0 && new_tsec->sid == ksu_sid)\n            return 0;\n    }'
+       security/selinux/hooks.c
+        ;;
+    esac
+
+done
